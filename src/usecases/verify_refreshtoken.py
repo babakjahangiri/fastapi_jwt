01960f552bc5af040f837da9ef8786242f1bf34e
Babak Jahangiri <babakjahangiri@gmail.com>
2023-08-21 18:47:04 +0100

    add usecase bodies

---
+++ src/usecases/verify_refreshtoken.py
@@ -0,0 +1,9 @@
+from src.db.user_db import fake_users_db
+
+
+class VerifyRefreshtoken:
+    def __init__(self) -> None:
+        self.db = fake_users_db
+
+    def execute(self) -> None:
+        pass
