9900c56ca3d2c4c8254257e2adcec3e473320d34
Arshiya-python-developer <arshiya.pogrammer@gmail.com>
2023-08-22 13:43:47 +0330

    some changes

--- src/db/user_db.py
+++ src/db/user_db.py
@@ -1,13 +1,13 @@
 fake_users_db = {
-#    "babak": {
-#         "username": "babak",
-#         "full_name": "Babak Jahangiri",
-#         "email": "babak@babak.uk",
-#         "plain_password": "babak1234",  # Plain password
-#         "hashed_password": "$2b$12$MknkghNUO3nxjMnsQPcB2.DaDAcuDzOeTrqP/j9voB12ohPRYG6yu",
-#         "role": "admin",
-#         "disabled": False,
-#     },
+   "babak": {
+        "username": "babak",
+        "full_name": "Babak Jahangiri",
+        "email": "babak@babak.uk",
+        "plain_password": "babak1234",  # Plain password
+        "hashed_password": "$2y$10$A6JeQdXR0.lymWDSzRuEW.LcIdjhi0aXmknfCnT0cfQF//dB/rUk2",
+        "role": "admin",
+        "disabled": False,
+    },
     "arshiya": {
         "username": "arshiya",
         "full_name": "Arshiya Khalili",
@@ -17,12 +17,13 @@ fake_users_db = {
         "role": "user",
         "disabled": False,
     },
-    # "anonymous": {
-    #     "username": "anonymous",
-    #     "full_name": "anonymous anonymous",
-    #     "email": "anonymous@gmail.com",
-    #     "hashed_password": "$2b$12$MknkghNUO3nxjMnsQPcB3.DaDAcuDzOeTrqP/j9voB12ohPRYG7yu",
-    #     "role": "user",
-    #     "disabled": True,
-    # },
+    "anonymous": {
+         "username": "anonymous",
+        "full_name": "anonymous",
+        "email": "anonymous@gmail.com",
+        "plain_password": "anonymous",  # Plain password
+        "hashed_password": "$2y$10$AQfbLRB0NxBvg.L0GMkgv.OV5RhXLK0PJ6sNH8FR0bz2LROUagWkq",
+        "role": "user",
+        "disabled": False,
+    },
 }
