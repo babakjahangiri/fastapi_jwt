9076c0fb4a2c14432452d014dd5f7c76c99acf79
Mohsen Hassani <m.hassani.de@gmail.com>
2023-09-03 11:46:43 +0200

    Remove plain text password from user_db

--- src/db/user_db.py
+++ src/db/user_db.py
@@ -1,12 +1,8 @@
-from uuid import uuid4
-from pprint import pprint
-
 fake_users_db = {
    "babak": {
         "username": "babak",
         "full_name": "Babak Jahangiri",
         "email": "babak@babak.uk",
-        "plain_password": "babak1234",  # Plain password
         "hashed_password": "$2y$10$A6JeQdXR0.lymWDSzRuEW.LcIdjhi0aXmknfCnT0cfQF//dB/rUk2",
         "role": "admin",
         "disabled": False,
@@ -17,7 +13,6 @@ fake_users_db = {
         "username": "arshiya",
         "full_name": "Arshiya Khalili",
         "email": "Arshiya@gmail.com",
-        "plain_password": "arshiya", 
         "hashed_password": "$2y$10$xevgOpWiwYXCZ1VXECSd/OptJE3Dq3Lo6wqFLhu7jN/zmw6Ir0WpS",
         "role": "user",
         "disabled": False,
@@ -28,7 +23,6 @@ fake_users_db = {
          "username": "anonymous",
         "full_name": "anonymous",
         "email": "anonymous@gmail.com",
-        "plain_password": "anonymous",  
         "hashed_password": "$2y$10$AQfbLRB0NxBvg.L0GMkgv.OV5RhXLK0PJ6sNH8FR0bz2LROUagWkq",
         "role": "user",
         "disabled": False,
@@ -36,5 +30,3 @@ fake_users_db = {
         "refresh_token": None
     },
 }
-
-# pprint(fake_users_db)
