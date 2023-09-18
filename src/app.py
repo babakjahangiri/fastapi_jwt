c0e6ddebac50992deed6fa5f563dfac5188d629b
Arshiya-python-developer <arshiya.pogrammer@gmail.com>
2023-09-18 18:46:44 +0330

    add csrf token generator and validator

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
+import secrets
+from fastapi import Depends, FastAPI, Header, HTTPException, Request, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from src.auth.dependencies import CurrentActiveUser
@@ -15,11 +16,21 @@ from src.usecases.reset_password import ResetPassword
 from src.usecases.verify_refreshtoken import VerifyRefreshtoken
 
 
-
 app = FastAPI()
+
+user_csrf_tokens = {}
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def validate_csrf_token(request: Request, user_token: str = None):
+    received_token = request.headers.get("X-CSRF-Token", user_token)
+    
+    if received_token not in user_csrf_tokens.values():
+        raise HTTPException(status_code=400, detail="Invalid CSRF Token")
+    
+    return received_token
 
 
 @app.get("/")
@@ -105,3 +116,18 @@ def reset_password(userame:str,current_password:str, new_password:str):
 def verify_refresh_token(token: str):
     verify_token = VerifyRefreshtoken()
     return verify_token.execute(token)
+
+
+
+@app.get("/generate-csrf-token")
+def generate_csrf_token(response: Response):
+    user_id = "some_user_id"  
+    csrf_token = secrets.token_hex(16)
+    user_csrf_tokens[user_id] = csrf_token
+    
+    response.set_cookie(key="csrf_token", value=csrf_token, samesite="lax", httponly=True)
+    return {"message": "CSRF token generated and set as cookie"}
+
+@app.post("/some-endpoint")
+def protected_endpoint(token: str = Depends(validate_csrf_token)):
+    return {"message": "Request processed successfully"}
